3ff35a581adc286720e1d13635dbaf10b27e388f
Oliver <oliverjohnkim@icloud.com>
2026-07-25 14:34:06 -0500

    first steps

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # ScrASCII
+
 An ASCII Scratch renderer; for the terminal!
 
+## Features
+
+- ASCII renderer
+- Unicode half-block renderer
+- ANSI and True Color support!
+
 # License
+
 MPL-2.0. If you need to see it look at [LICENSE](./LICENSE)
---
+++ oxfmt.config.js
@@ -0,0 +1,35 @@
+export default {
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  printWidth: 100,
+  overrides: [
+    {
+      files: ['*.md'],
+      options: {
+        proseWrap: 'always',
+      },
+    },
+    {
+      files: ['*.json'],
+      options: {
+        tabWidth: 2,
+        printWidth: 120,
+      },
+    },
+    {
+      files: ['*.html', '*.css', '*.scss'],
+      options: {
+        tabWidth: 2,
+        printWidth: 120,
+      },
+    },
+  ],
+  ignorePatterns: ['pnpm-lock.yaml'],
+}
